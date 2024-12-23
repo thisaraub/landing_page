8aefd3bab14f3679d85a8929079d3752ed4aa4a3
Thisara Niriella <thisara.niriella@uzabase.com>
2024-12-23 17:11:34 +0530

    updated with code

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("thisara")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    # Render the HTML template for the landing page
+    return render_template("template.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
